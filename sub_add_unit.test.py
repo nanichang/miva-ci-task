5033cfbed18ac0684136c270e8140a5b6f06cba0
Solober <solomon.chituru@miva.edu.ng>
2024-10-05 21:13:00 +0100

    updated workflow

--- sub_add_unit.test.py
+++ sub_add_unit.test.py
@@ -2,10 +2,10 @@ import unittest
 
 class TestMathOperations(unittest.TestCase):
     def test_addition(self):
-        self.assertEqual(2 + 3, 5)
+        self.assertEqual(2 + 8, 10)
 
     def test_subtraction(self):
-        self.assertEqual(5 - 3, 2)
+        self.assertEqual(5 - 4, 1)
 
 if __name__ == '__main__':
     unittest.main()
